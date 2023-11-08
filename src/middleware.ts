30eac1481c86644c25d1d7bb43fd7df22fc3b6ee
daniel Mayo <daniielmayo.mayo@gmail.com>
2023-11-08 14:43:30 -0500

    update func middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,22 @@
-export { default } from "next-auth/middleware";
+
+import { getToken } from "next-auth/jwt";
 import { NextResponse } from 'next/server'
 import type { NextRequest } from 'next/server'
 
-export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL('/', request.url))
+export async function middleware(req: NextRequest) {
+
+  const session = await getToken({req, secret:process.env.NEXTAUTH_SECRET})
+
+  console.log({session});
+  if(!session){
+    const requestedPage = req.nextUrl.pathname
+    const url = req.nextUrl.clone()
+    url.pathname=  `/loginPage`
+    url.search= `${requestedPage}`
+    return NextResponse.redirect(url)
+  }
+ 
+  return NextResponse.next()
 }
 export const config = {
   matcher: ["/dashboard/:path*"],
