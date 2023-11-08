2879cdf1b2adbfd509e70f8e173143fa8f364b18
daniel Mayo <daniielmayo.mayo@gmail.com>
2023-11-08 14:44:39 -0500

    delete console middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -7,7 +7,6 @@ export async function middleware(req: NextRequest) {
 
   const session = await getToken({req, secret:process.env.NEXTAUTH_SECRET})
 
-  console.log({session});
   if(!session){
     const requestedPage = req.nextUrl.pathname
     const url = req.nextUrl.clone()
