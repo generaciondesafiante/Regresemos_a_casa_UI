608cc5b9f587b43aef7ccb2479cc99dd37f914ae
danielmayo <daniielmayo.mayo.com>
2023-11-21 08:31:54 -0500

    Changecomment explaining the content of the page learning path of user information messag

--- components/organisms/LearningPath/LearningPath.tsx
+++ components/organisms/LearningPath/LearningPath.tsx
@@ -1,5 +1,6 @@
 export const LearningPath = () => {
   return (
+    // This lorem ipsum is to fill in the content of the page, it will be changed as soon as you work on the card corresponding to this page
     <div>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit provident
       quae vel, veniam iusto maiores et veritatis sint cum exercitationem labore
