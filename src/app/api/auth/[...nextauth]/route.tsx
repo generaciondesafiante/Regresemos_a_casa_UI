27e82d0d4ebd1d63f06e95ecd961093db57ce13a
danielmayo <daniielmayo.mayo.com>
2023-12-12 09:47:08 -0500

    runtime implementation in the file route

--- src/app/api/auth/[...nextauth]/route.tsx
+++ src/app/api/auth/[...nextauth]/route.tsx
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import Swal from "sweetalert2";
+
+export const runtime = "edge";
 const handler = NextAuth({
   providers: [
     CredentialsProvider({
