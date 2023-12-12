c858b63dcb12cbac92f73c730596b7e1377efc06
Cristian Arce <cristiancamilo_1989@hotmail.com>
2023-12-12 09:43:13 -0500

    assessment styles

--- components/organisms/Assessment/AssessmentQuestions/AssessmentQuestions.tsx
+++ components/organisms/Assessment/AssessmentQuestions/AssessmentQuestions.tsx
@@ -1,67 +1,196 @@
 "use client";
 import styles from "./AssessmentQuestions.module.css";
+import { useState, useEffect } from 'react'
 
 const questions = [
-    {
-        title: "¿ Cuál es el primogenito de abraham ?",
-        options: [
-            { textAnswer: "Jose", isCorrect: false },
-            { textAnswer: "Ismael", isCorrect: false },
-            { textAnswer: "Isaac", isCorrect: true },
-            { textAnswer: "Jacob", isCorrect: false },
-        ],
-    },
-    {
-        title: "¿ Cuál es el primogenito de abraham 2?",
-        options: [
-            { textAnswer: "Jose2", isCorrect: false },
-            { textAnswer: "Ismael2", isCorrect: false },
-            { textAnswer: "Isaac2", isCorrect: true },
-            { textAnswer: "Jacob2", isCorrect: false },
-        ],
-    },
-    {
-        title: "¿ Abraham significa... Padre ?",
-        options: [
-            { textAnswer: "Falso", isCorrect: true },
-            { textAnswer: "Verdadero", isCorrect: false },
-        ],
-    },
-    {
-        title: "¿ Cuáles fueron las promesas que le dio Dios ?",
-        options: [
-            { textAnswer: "Descendencia como las estrellas de la arena", isCorrect: true },
-            { textAnswer: "Benditas en ti todas las familias de la tierra", isCorrect: true },
-            { textAnswer: "Un territorio que fluye leche y miel", isCorrect: true },
-            { textAnswer: "Que tendria varias esposas", isCorrect: false },
-        ],
-    },
+  {
+    title: "¿ Cuál es el primogenito de abraham ?",
+    image: "esto es una imagen",
+    options: [
+      { textAnswer: "Jose", isCorrect: false },
+      { textAnswer: "Ismael", isCorrect: false },
+      { textAnswer: "Isaac", isCorrect: true },
+      { textAnswer: "Jacob", isCorrect: false },
+    ],
+  },
+  {
+    title: "¿ Cuáles son los hijos de abraham ?",
+    image: "esto es una imagen",
+    options: [
+      { textAnswer: "cristian", isCorrect: false },
+      { textAnswer: "daniel", isCorrect: false },
+      { textAnswer: "Isaac", isCorrect: true },
+      { textAnswer: "Ismael", isCorrect: true },
+    ],
+  },
+  {
+    title: "¿ Cuáles son los hijos de abraham2 ?",
+    image: "esto es una imagen",
+    options: [
+      { textAnswer: "cristian2", isCorrect: false },
+      { textAnswer: "daniel2", isCorrect: false },
+      { textAnswer: "Isaac2", isCorrect: true },
+      { textAnswer: "Ismael2", isCorrect: true },
+    ],
+  },
+  {
+    title: "¿ Cuál es el primogenito de abraham 2?",
+    image: "esto es una imagen",
+    options: [
+      { textAnswer: "Jose2", isCorrect: false },
+      { textAnswer: "Ismael2", isCorrect: false },
+      { textAnswer: "Isaac2", isCorrect: true },
+      { textAnswer: "Jacob2", isCorrect: false },
+    ],
+  },
+  {
+    title: "¿ Cuál es el primogenito de abraham ?",
+    image: "esto es una imagen",
+    options: [
+      { textAnswer: "Jose", isCorrect: false },
+      { textAnswer: "Ismael", isCorrect: false },
+      { textAnswer: "Isaac", isCorrect: true },
+      { textAnswer: "Jacob", isCorrect: false },
+    ],
+  },
+  {
+    title: "¿ Cuál es el primogenito de abraham ?",
+    image: "esto es una imagen",
+    options: [
+      { textAnswer: "Jose", isCorrect: false },
+      { textAnswer: "Ismael", isCorrect: false },
+      { textAnswer: "Isaac", isCorrect: true },
+      { textAnswer: "Jacob", isCorrect: false },
+    ],
+  },
+  {
+    title: "¿ Abraham significa... Padre ?",
+    image: "esto es una imagen",
+    options: [
+      { textAnswer: "Falso", isCorrect: true },
+      { textAnswer: "Verdadero", isCorrect: false },
+    ],
+  },
+  {
+    title: "¿ Abraham significa... Padre ?",
+    image: "esto es una imagen",
+    options: [
+      { textAnswer: "Falso", isCorrect: true },
+      { textAnswer: "Verdadero", isCorrect: false },
+    ],
+  },
+  {
+    title: "¿ Abraham significa... Padre ?",
+    image: "esto es una imagen",
+    options: [
+      { textAnswer: "Falso", isCorrect: true },
+      { textAnswer: "Verdadero", isCorrect: false },
+    ],
+  },
+  {
+    title: "¿ Cuáles fueron las promesas que le dio Dios ?",
+    image: "esto es una imagen",
+    options: [
+      { textAnswer: "Descendencia como las estrellas de la arena", isCorrect: true },
+      { textAnswer: "Benditas en ti todas las familias de la tierra", isCorrect: true },
+      { textAnswer: "Un territorio que fluye leche y miel", isCorrect: true },
+      { textAnswer: "Que tendria varias esposas", isCorrect: false },
+    ],
+  },
 ]
 
 export const AssessmentQuestions = () => {
-    return (
-        <div className={styles["assessmentQuestions__container--questions"]}>
-            <section>
-                <div className={styles["assessmentQuestions__img"]}></div>
-            </section>
-            <section>
-                <p className={styles["assessmentQuestions__numberQuestion"]}>1 de 10</p>
-            </section>
-            <div className={styles["assessmentQuestions__container--buttons"]}>
-                    <section className={styles["assessmentQuestions__content--titleQuestion"]}>
-                        <span className={styles["assessmentQuestions__titleQuestion"]}>¿ Cuál es el primogénito de Abraham ?</span>
-                    </section>
-                    <section className={styles["assessmentQuestions__content--buttons"]}>
-                        <div className={styles["assessmentQuestions__row"]}>
-                            <button className={`${styles["assessmentQuestions__button"]} ${styles["button1"]}`}>Opcion 1</button>
-                            <button className={`${styles["assessmentQuestions__button"]} ${styles["button2"]}`}>Opcion 2</button>
-                        </div>
-                        <div className={styles["assessmentQuestions__row"]}>
-                            <button className={`${styles["assessmentQuestions__button"]} ${styles["button3"]}`}>Opcion 3</button>
-                            <button className={`${styles["assessmentQuestions__button"]} ${styles["button4"]}`}>Opcion 4</button>
-                        </div>
-                    </section>
-            </div>
-        </div>
-    )
-}
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [score, setScore] = useState(0);
+  const [selectedButtonIndex, setSelectedButtonIndex] = useState<number | null>(null);
+  const [correctButtonIndex, setCorrectButtonIndex] = useState<number | null>(null);
+
+  useEffect(() => {
+    // Verificar si la pregunta actual tiene solo una respuesta correcta
+    const currentOptions = questions[currentQuestion].options;
+    const countCorrect = currentOptions.filter((option) => option.isCorrect).length;
+    
+    // Deshabilitar botones después de seleccionar uno si hay solo un valor "true"
+    if (countCorrect === 1 && selectedButtonIndex !== null) {
+      setCorrectButtonIndex(
+        currentOptions.findIndex((option) => option.isCorrect)
+      );
+    }
+  }, [currentQuestion, selectedButtonIndex]);
+
+  const isAnswerCorrect = (index: number) => {
+    return questions[currentQuestion].options[index].isCorrect;
+  };
+
+  const handleAnswerSubmit = (index: number) => {
+    setSelectedButtonIndex(index);
+
+    const isCorrect = isAnswerCorrect(index);
+    if (isCorrect) {
+      setScore((prevScore) => prevScore + 1);
+    } else {
+      setCorrectButtonIndex(
+        questions[currentQuestion].options.findIndex((option) => option.isCorrect)
+      );
+    }
+  };
+
+  const advanceToNextQuestion = () => {
+    if (currentQuestion === questions.length - 1) {
+      console.log("Assessment finalizado");
+    } else {
+      setCurrentQuestion((prev) => prev + 1);
+      setSelectedButtonIndex(null);
+      setCorrectButtonIndex(null);
+    }
+  };
+
+  return (
+    <div className={styles["assessmentQuestions__container--questions"]}>
+      <section>
+        <div className={styles["assessmentQuestions__img"]}></div>
+      </section>
+      <section>
+        <p className={styles["assessmentQuestions__numberQuestion"]}>{currentQuestion + 1} de {questions.length} </p>
+      </section>
+      <div className={styles["assessmentQuestions__container--buttons"]}>
+        <section className={styles["assessmentQuestions__content--titleQuestion"]}>
+          <span className={styles["assessmentQuestions__titleQuestion"]}>{questions[currentQuestion].title}</span>
+        </section>
+        <section className={styles["assessmentQuestions__content--buttons"]}>
+          <div className={styles['assessmentQuestions__row']}>
+            {questions[currentQuestion].options.map((answer, index) => (
+              <button
+              key={answer.textAnswer}
+            className={`${styles['assessmentQuestions__button']} ${styles['button' + (index + 1)]} ${
+              selectedButtonIndex !== null
+                ? index === selectedButtonIndex
+                  ? isAnswerCorrect(index)
+                    ? styles['correct']
+                    : styles['incorrect']
+                  : index === correctButtonIndex
+                    ? styles['correct']
+                    : styles['inactive']
+                : styles['active']
+            }`}
+            onClick={() => handleAnswerSubmit(index)}
+            disabled={selectedButtonIndex !== null}
+          >
+            {answer.textAnswer}
+              </button>
+            ))}
+          </div>
+        </section>
+      </div>
+      <section>
+        <p>Puntuación: {score}</p>
+        <button
+          onClick={advanceToNextQuestion}
+          disabled={selectedButtonIndex === null} // Deshabilitar el botón hasta que se seleccione una respuesta
+        >
+          Siguiente
+        </button>
+      </section>
+    </div>
+  );
+};
