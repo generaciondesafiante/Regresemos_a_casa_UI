19c19f2e0adf1b1baadc639891016e359c89bade
danielmayo <daniielmayo.mayo.com>
2024-01-24 16:50:43 -0500

    update state of the phone

--- components/organisms/Register/Register.tsx
+++ components/organisms/Register/Register.tsx
@@ -20,7 +20,7 @@ export const Register = () => {
   const [lastname, setLastName] = useState<string>("");
   const [country, setCountry] = useState<string>("");
   const [city, setCity] = useState<string>("");
-  const [phone, setPhone] = useState<number | null>(null);
+  const [phone, setPhone] = useState<number>();
   const [image, setImage] = useState<string>(
     "http://somebooks.es/wp-content/uploads/2018/12/Poner-una-imagen-a-la-cuenta-de-usuario-en-Windows-10-000.png"
   );
