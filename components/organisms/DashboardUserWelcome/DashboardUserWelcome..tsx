4177978bfbd7142ff3017c8544475cb0620f441b
danielmayo <daniielmayo.mayo.com>
2024-01-30 13:34:11 -0500

    If the user has not seen videos, recommend certain videos to start consuming content

--- components/organisms/DashboardUserWelcome/DashboardUserWelcome..tsx
+++ components/organisms/DashboardUserWelcome/DashboardUserWelcome..tsx
@@ -21,10 +21,14 @@ export const DashboardUserWelcome = () => {
   const router = useRouter();
   const idUser = session?.user.uid;
   const name = session?.user?.name;
-
+  const urlVideoDefault =
+    "https://www.youtube.com/embed/CovSIgAtFIs?si=kofztRWT519UyJug";
+  const routeVideoDefault =
+    "/dashboard/courses/curso_basico_biblico/65b2a19a0b68c1de28c171c2/04306687-b6b4-49eb-95fd-f153de3c2fb2/introducción/1";
   const [data, setData] = useState<UserData | null>(null);
   const [isVideoReady, setisVideoReady] = useState(false);
-
+  const [isLoading, setIsLoading] = useState(true);
+  console.log(isVideoReady);
   const FILLER_CONTENT_IMG =
     "https://static.wixstatic.com/media/d166cc_4cc837baf9254000a0f3963193c6b07a~mv2.jpg/v1/fill/w_368,h_195,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Romanos%2011111.jpg";
 
@@ -43,23 +47,28 @@ export const DashboardUserWelcome = () => {
         );
 
         if (response.ok) {
-          setTimeout(() => {
-            setisVideoReady(true);
-          }, 800);
           const dataUser = await response.json();
-          setData(dataUser.lastViewedInfo);
+
+          if (
+            dataUser.lastViewedInfo &&
+            Object.keys(dataUser.lastViewedInfo).length > 0
+          ) {
+            setData(dataUser.lastViewedInfo);
+            setisVideoReady(true);
+          } else {
+            setisVideoReady(false);
+          }
         } else {
           console.error(
             "Error al obtener la información del usuario:",
             response.statusText
           );
-
-          setTimeout(() => {
-            setisVideoReady(false);
-          }, 800);
+          setisVideoReady(false);
         }
       } catch (error) {
         console.error("Error al realizar la solicitud fetch:", error);
+      } finally {
+        setIsLoading(false);
       }
     };
     userInformacion();
@@ -82,34 +91,61 @@ export const DashboardUserWelcome = () => {
           }
         />
       </div>
-      <h3 className={styles["dashboardUserWelcome-subtitle"]}>
-        ¡Continúa tu curso!
-      </h3>
-      {isVideoReady ? (
-        <div
-          className={styles["dashboardUserWelcome-continuePlayingContainer"]}
-        >
-          <ReactPlayer
-            onClick={() =>
-              router.push(
-                `/dashboard/courses/${data?.courseName}/${data?.idCourse}/${data?.idVideo}/${data?.tema}/${data?.indexTopic}`
-              )
-            }
-            url={data?.urlVideo}
-            controls={true}
-            playsinline={true}
-            pip={true}
-            stopOnUnmount
-            width={"100%"}
-            height={"100%"}
-            light={true}
-            className={styles["dashboardUserWelcome-continuePlaying"]}
-          />
+
+      {isLoading ? (
+        <div>
+          <div className={styles["titleVideo-skeletonVideo"]}> </div>
+          <div className={styles["lastViewedVideo-skeletonVideo"]}></div>
+        </div>
+      ) : isVideoReady ? (
+        <div>
+          <h3 className={styles["dashboardUserWelcome-subtitle"]}>
+            ¡Continúa tu curso!
+          </h3>
+          <div
+            className={styles["dashboardUserWelcome-continuePlayingContainer"]}
+          >
+            <ReactPlayer
+              onClick={() =>
+                router.push(
+                  `/dashboard/courses/${data?.courseName}/${data?.idCourse}/${data?.idVideo}/${data?.tema}/${data?.indexTopic}`
+                )
+              }
+              url={data?.urlVideo}
+              controls={true}
+              playsinline={true}
+              pip={true}
+              stopOnUnmount
+              width={"100%"}
+              height={"100%"}
+              light={true}
+              className={styles["dashboardUserWelcome-continuePlaying"]}
+            />
+          </div>
         </div>
       ) : (
-        <div className={styles["lastViewedVideo-skeletonVideo"]}></div>
+        <div>
+          <h3 className={styles["dashboardUserWelcome-subtitle"]}>
+            ¡Inicia el recorrido de la fé!
+          </h3>
+          <div
+            className={styles["dashboardUserWelcome-continuePlayingContainer"]}
+          >
+            <ReactPlayer
+              onClick={() => router.push(routeVideoDefault)}
+              url={urlVideoDefault}
+              controls={true}
+              playsinline={true}
+              pip={true}
+              stopOnUnmount
+              width={"100%"}
+              height={"100%"}
+              light={true}
+              className={styles["dashboardUserWelcome-continuePlaying"]}
+            />
+          </div>
+        </div>
       )}
-
       <h3 className={styles["dashboardUserWelcome-subtitle"]}>
         Recursos para ti
       </h3>
