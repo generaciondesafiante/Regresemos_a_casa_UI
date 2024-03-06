a189a7760ff9489c6b3c51c42610bf617faa8c5a
Sthefania Echeverry Orozco <sthefaniaecheverry@gmail.com>
2024-03-06 10:08:02 -0500

    Changing PDFView

---
+++ src/app/(privateRoutes)/dashboard/PDFView/page.tsx
@@ -0,0 +1,9 @@
+import { PDFViewTemplate } from '../../../../../components/templates/PDFView-template/PDFViewTemplate';
+
+const page = () => {
+    return (
+        <PDFViewTemplate />
+    )
+}
+
+export default page
--- src/app/(privateRoutes)/dashboard/viewpdf/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ViewpdfTemplate } from '../../../../../components/templates/Viewpdf-template/ViewpdfTemplate';
-
-const page = () => {
-    return (
-        <ViewpdfTemplate />
-    )
-}
-
-export default page
