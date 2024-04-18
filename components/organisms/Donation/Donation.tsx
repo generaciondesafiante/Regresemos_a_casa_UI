36a52b29874a9282cd373c818f7403140e508a7f
daniel mayo <daniielmayo.mayo@gmail.com>
2024-04-18 12:23:58 -0500

    changing styles and renaming classes

--- components/organisms/Donation/Donation.tsx
+++ components/organisms/Donation/Donation.tsx
@@ -19,53 +19,52 @@ export const Donation = () => {
   ];
 
   return (
-    <section className={styles["container-donation"]}>
-      <div className={styles["container-info-donation-background"]}>
-        <div className={styles["container-info-dontaion"]}>
-          <div className={styles["content-info"]}>
-            <h2 className={styles["title-info"]}>
-              SE PARTE DE UNA GENERACIÓN DESAFIANTE.
-            </h2>
-            <p className={styles["paragraph-info"]}>
-              Este contenido es totalmente gratuito, tratamos de difundir la
-              palabra del Señor a los cuatro ángulos de la tierra. Tomamos la
-              palabra que decía Pablo en su Carta a Gálatas “El que es enseñado
-              en la palabra haga participe de toda cosa buena al que lo
-              instruye”.
-            </p>
-            <p className={styles["paragraph-info"]}>
-              Gracias por invertir y apoyar a este ministerio con
-              <b> amor y gozo. Y lo mejor aún</b>, ser parte del esparcimiento
-              de las escrituras.
-            </p>
+    <section className={styles["donation-container"]}>
+      <div className={styles["donation-information_backgroundContainer"]}>
+        <div className={styles["donation-info_container"]}>
+        <div className={styles["info-content"]}>
+          <h2 className={styles["title-info"]}>
+            SE PARTE DE UNA GENERACIÓN DESAFIANTE.
+          </h2>
+          <p className={styles["paragraph-info"]}>
+            Este contenido es totalmente gratuito, tratamos de difundir la
+            palabra del Señor a los cuatro ángulos de la tierra. Tomamos la
+            palabra que decía Pablo en su Carta a Gálatas “El que es enseñado en
+            la palabra haga participe de toda cosa buena al que lo instruye”.
+          </p>
+          <p className={styles["paragraph-info"]}>
+            Gracias por invertir y apoyar a este ministerio con
+            <b> amor y gozo. Y lo mejor aún</b>, ser parte del esparcimiento de
+            las escrituras.
+          </p>
+        </div>
+        <div className={styles["content-information_payment-methods"]}>
+          <p className={styles["payment-methods_paragraph"]}>
+            Puedes invertir directamente de PayPal.
+          </p>
+          <div className={styles["methods-button_content"]}>
+            <Button className={`${styles["paypalme-button"]}`}>
+              Paypal.me
+            </Button>
+            <Button className={` ${styles["support-button"]}`}>
+              Apoya Aquí
+            </Button>
           </div>
-          <div className={styles["content-payment-methods_info"]}>
-            <p className={styles["paragraph-payment-methods"]}>
-              Puedes invertir directamente de PayPal.
-            </p>
-            <div className={styles["content-buttons-payment"]}>
-              <Button className={`${styles["button-paypal-me"]}`}>
-                Paypal.me
-              </Button>
-              <Button className={` ${styles["button-support"]}`}>
-                Apoya Aquí
-              </Button>
-            </div>
-            <div className={styles["payment-methods_cards"]}>
-              {cards.map((card, index) => (
-                <img src={card.image} alt={card.title} key={index} />
-              ))}
-            </div>
+          <div className={styles["payment-methods_cards"]}>
+            {cards.map((card, index) => (
+              <img src={card.image} alt={card.title} key={index} />
+            ))}
+          </div>
           </div>
         </div>
       </div>
-      <div className={styles["container-deposit"]}>
-        <h2 className={styles["title-deposit"]}>Depositos</h2>
-        <h3 className={styles["title-bank"]}>Banco Santander</h3>
+      <div className={styles["deposit-container"]}>
+        <h2 className={styles["deposit-title"]}>Depositos</h2>
+        <h3 className={styles["bank-title"]}>Banco Santander</h3>
         {deposit.map((data, index) => (
-          <div key={index} className={styles["content-data-banck"]}>
-            <h4 className={styles["title-data"]}>{data.title}</h4>
-            <p className={styles["info-data"]}>{data.info}</p>
+          <div key={index} className={styles["bank-data_content"]}>
+            <p className={styles["data-title"]}>{data.title}</p>
+            <p className={styles["data-info"]}>{data.info}</p>
           </div>
         ))}
       </div>
