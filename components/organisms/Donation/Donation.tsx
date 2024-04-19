99a11f0f66d2f44511d67c868bda3aa61a188ce1
daniel mayo <daniielmayo.mayo@gmail.com>
2024-04-19 12:28:43 -0500

    change name class for method BEM

--- components/organisms/Donation/Donation.tsx
+++ components/organisms/Donation/Donation.tsx
@@ -19,52 +19,53 @@ export const Donation = () => {
   ];
 
   return (
-    <section className={styles["donation-container"]}>
-      <div className={styles["donation-information_backgroundContainer"]}>
-        <div className={styles["donation-info_container"]}>
-        <div className={styles["info-content"]}>
-          <h2 className={styles["title-info"]}>
-            SE PARTE DE UNA GENERACIÓN DESAFIANTE.
-          </h2>
-          <p className={styles["paragraph-info"]}>
-            Este contenido es totalmente gratuito, tratamos de difundir la
-            palabra del Señor a los cuatro ángulos de la tierra. Tomamos la
-            palabra que decía Pablo en su Carta a Gálatas “El que es enseñado en
-            la palabra haga participe de toda cosa buena al que lo instruye”.
-          </p>
-          <p className={styles["paragraph-info"]}>
-            Gracias por invertir y apoyar a este ministerio con
-            <b> amor y gozo. Y lo mejor aún</b>, ser parte del esparcimiento de
-            las escrituras.
-          </p>
-        </div>
-        <div className={styles["content-information_payment-methods"]}>
-          <p className={styles["payment-methods_paragraph"]}>
-            Puedes invertir directamente de PayPal.
-          </p>
-          <div className={styles["methods-button_content"]}>
-            <Button className={`${styles["paypalme-button"]}`}>
-              Paypal.me
-            </Button>
-            <Button className={` ${styles["support-button"]}`}>
-              Apoya Aquí
-            </Button>
+    <section className={styles["donations"]}>
+      <div className={styles["donations-backgroundContainer_info"]}>
+        <div className={styles["donations-container_info"]}>
+          <div className={styles["donations-content_info"]}>
+            <h2 className={styles["donations-title_info"]}>
+              SE PARTE DE UNA GENERACIÓN DESAFIANTE.
+            </h2>
+            <p className={styles["donations-paragraph_info"]}>
+              Este contenido es totalmente gratuito, tratamos de difundir la
+              palabra del Señor a los cuatro ángulos de la tierra. Tomamos la
+              palabra que decía Pablo en su Carta a Gálatas “El que es enseñado
+              en la palabra haga participe de toda cosa buena al que lo
+              instruye”.
+            </p>
+            <p className={styles["donations-paragraph_info"]}>
+              Gracias por invertir y apoyar a este ministerio con
+              <b> amor y gozo. Y lo mejor aún</b>, ser parte del esparcimiento
+              de las escrituras.
+            </p>
           </div>
-          <div className={styles["payment-methods_cards"]}>
-            {cards.map((card, index) => (
-              <img src={card.image} alt={card.title} key={index} />
-            ))}
-          </div>
+          <div className={styles["donations-informations_paymentMethods"]}>
+            <p className={styles["donations-paragraph_pymentMethods"]}>
+              Puedes invertir directamente de PayPal.
+            </p>
+            <div className={styles["donations-contentButton_paymentMethods"]}>
+              <Button className={`${styles["donations-button_pypalme"]}`}>
+                Paypal.me
+              </Button>
+              <Button className={` ${styles["donations-button_support"]}`}>
+                Apoya Aquí
+              </Button>
+            </div>
+            <div className={styles["donations-cards_methods"]}>
+              {cards.map((card, index) => (
+                <img src={card.image} alt={card.title} key={index} />
+              ))}
+            </div>
           </div>
         </div>
       </div>
-      <div className={styles["deposit-container"]}>
-        <h2 className={styles["deposit-title"]}>Depositos</h2>
-        <h3 className={styles["bank-title"]}>Banco Santander</h3>
+      <div className={styles["donations-container_deposit"]}>
+        <h2 className={styles["donations-title_deposit"]}>Depositos</h2>
+        <h3 className={styles["donations-title_bank"]}>Banco Santander</h3>
         {deposit.map((data, index) => (
-          <div key={index} className={styles["bank-data_content"]}>
-            <p className={styles["data-title"]}>{data.title}</p>
-            <p className={styles["data-info"]}>{data.info}</p>
+          <div key={index} className={styles["donations-content_dataBank"]}>
+            <p className={styles["donations-title_data"]}>{data.title}</p>
+            <p className={styles["donations-info_data"]}>{data.info}</p>
           </div>
         ))}
       </div>
