2b82d111d74df07061fd217f6aa86d33ea0255a9
Daniielmayo <79063734+Daniielmayo@users.noreply.github.com>
2024-05-02 17:34:41 -0500

    Des 356 fe move estudios estudios tematicos section responsive (#150)
    * create page thematics Studies
    
    * create section images of the themaatic studies
    
    * change from old CSS methodology to BEM methodology
    
    * chandge of the styles in the component
    
    * change fo the styles component with PR

---
+++ components/organisms/ThematicStudies/dataPDF.ts
@@ -0,0 +1,55 @@
+interface ThematicStudy {
+  name: string;
+  pdfLink: string;
+  image: string;
+  description?: string;
+}
+
+export const thematicsStudiesPDF: ThematicStudy[] = [
+  {
+    name: "ATRAÍDOS ",
+    description: "Como en una historia de amor, así el Señor nos atrae.",
+    pdfLink:
+      "https://firebasestorage.googleapis.com/v0/b/pdf-resouraces.appspot.com/o/Estudios-tematicos%2FAtraidos3_compressed.pdf?alt=media&token=492a10c5-c091-497f-98aa-48e46158f2a8",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/pdf-resouraces.appspot.com/o/Estudios-tematicos%2FimagePDF%2Fatraido.webp?alt=media&token=b04c058a-8693-49ab-94a6-bc90783b4c59",
+  },
+
+  {
+    name: "CUMPLEAÑOS",
+    pdfLink:
+      "https://firebasestorage.googleapis.com/v0/b/pdf-resouraces.appspot.com/o/Estudios-tematicos%2FOrigen%20cumple.2.pdf?alt=media&token=88370b4f-1ec0-478d-9ed5-f9385c81311c",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/pdf-resouraces.appspot.com/o/Estudios-tematicos%2FimagePDF%2Fcumplea%C3%B1os.webp?alt=media&token=fac72e35-ca07-4159-a264-5bacd5568a72",
+  },
+  {
+    name: "PUREZA DESAFIANTE",
+    description: "Leyes de la menstruación",
+    pdfLink:
+      "https://firebasestorage.googleapis.com/v0/b/pdf-resouraces.appspot.com/o/Estudios-tematicos%2FPureza%20desafiante%2C%20leyes%20de%20menstruacion.pdf?alt=media&token=27ba771a-2c8e-4a98-a6e6-88651efe6551",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/pdf-resouraces.appspot.com/o/Estudios-tematicos%2FimagePDF%2Fleyesde%20la%20menstruacion.jpg?alt=media&token=481d80c8-4440-4ddc-b081-a86b8c6306a0",
+  },
+  {
+    name: "PUREZA DESAFIANTE",
+    description: "Pureza en el matrimonio.",
+    pdfLink:
+      "https://firebasestorage.googleapis.com/v0/b/pdf-resouraces.appspot.com/o/Estudios-tematicos%2FPureza%20desafiante%2C%20pureza%20matrimonio.pdf?alt=media&token=a5b172bc-8ebd-4f36-885c-fa1e6630a2e6",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/pdf-resouraces.appspot.com/o/Estudios-tematicos%2FimagePDF%2FpurezaEnMatrimonio.webp?alt=media&token=12fcd336-2f63-4810-a9d2-ee7a4955f70e",
+  },
+  {
+    name: "DÍA DE LAS MADRES",
+    pdfLink:
+      "https://firebasestorage.googleapis.com/v0/b/pdf-resouraces.appspot.com/o/Estudios-tematicos%2FOrigen_del_d%C3%ADa_de_la_madre.pdf?alt=media&token=4bb1b987-d747-45fe-b5df-2a1dcc6b061f",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/pdf-resouraces.appspot.com/o/Estudios-tematicos%2FimagePDF%2FdiaDeLasMadre.webp?alt=media&token=2a916493-287f-452f-ba93-079ab11bc67d",
+  },
+  {
+    name: "NAVIDAD",
+    pdfLink:
+      "https://firebasestorage.googleapis.com/v0/b/pdf-resouraces.appspot.com/o/Estudios-tematicos%2FNavidad.pdf?alt=media&token=fad50b98-a314-4913-b98b-bd353c01ede7",
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/pdf-resouraces.appspot.com/o/Estudios-tematicos%2FimagePDF%2FNavidad.webp?alt=media&token=b5079d17-67d7-4e5b-b8fd-e1f614c4df8a",
+  },
+];
