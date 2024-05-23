0f235c4fadf8266f97255e22c835471679031ccb
Daniielmayo <79063734+Daniielmayo@users.noreply.github.com>
2024-05-23 08:02:57 -0500

    Des 364 fe move grupos desafiantes salud desafiante (#159)
    * create page salud desafainte
    
    * create page "salud desafiante"
    
    * "salud desafiante"  page creation
    
    * comment solution

---
+++ components/organisms/ChallengingHealth/data/menus.ts
@@ -0,0 +1,56 @@
+export const dataMenuOne = [
+  {
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/saluddesafiante.appspot.com/o/menu%20semanal%2Fmenu-01.webp?alt=media&token=9b2b7050-0e5c-48c2-ba41-5ebdb61b51dd",
+    link: "https://firebasestorage.googleapis.com/v0/b/pdf-resouraces.appspot.com/o/Salud%20desafiante%2FMenu%2F01.pdf?alt=media&token=f15a9c19-9928-4807-814c-0b3d06dbfb65",
+  },
+  {
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/saluddesafiante.appspot.com/o/menu%20semanal%2Fmenu-02.webp?alt=media&token=11a6142a-ba21-4470-b8d7-cff2a409b987",
+    link: "https://firebasestorage.googleapis.com/v0/b/pdf-resouraces.appspot.com/o/Salud%20desafiante%2FMenu%2F02.pdf?alt=media&token=fa9dd9a2-bd97-4cec-9ed1-661c12320c92",
+  },
+
+  {
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/saluddesafiante.appspot.com/o/menu%20semanal%2Fmenu-03.webp?alt=media&token=6ee737da-6690-4f6e-94da-130cf755ea93",
+    link: "https://firebasestorage.googleapis.com/v0/b/pdf-resouraces.appspot.com/o/Salud%20desafiante%2FMenu%2F03.pdf?alt=media&token=6e174786-df1f-460b-bf43-086777d60461",
+  },
+  {
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/saluddesafiante.appspot.com/o/menu%20semanal%2Fmenu-04.webp?alt=media&token=669bc23a-21f7-4531-968b-5de14d08857b",
+    link: "https://firebasestorage.googleapis.com/v0/b/pdf-resouraces.appspot.com/o/Salud%20desafiante%2FMenu%2F04.pdf?alt=media&token=c6461f96-f530-4c3c-bae0-f9dfcec94216",
+  },
+  {
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/saluddesafiante.appspot.com/o/menu%20semanal%2Fmenu-05.webp?alt=media&token=cfb5dff7-c924-4b84-83e3-0dcc7fdea75c",
+    link: "https://firebasestorage.googleapis.com/v0/b/pdf-resouraces.appspot.com/o/Salud%20desafiante%2FMenu%2F05.pdf?alt=media&token=b6db21dd-f4bd-459f-a334-a11c85d9e7e5",
+  },
+  {
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/saluddesafiante.appspot.com/o/menu%20semanal%2Fmenu-06.webp?alt=media&token=9e0cea3d-b5dc-4cf8-8637-ba9c0bc09712",
+    link: "https://firebasestorage.googleapis.com/v0/b/pdf-resouraces.appspot.com/o/Salud%20desafiante%2FMenu%2F06.pdf?alt=media&token=8d760213-f6dc-49a3-a7e0-66d50f12589a",
+  },
+  {
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/saluddesafiante.appspot.com/o/menu%20semanal%2Fmenu-07.webp?alt=media&token=7e2b5de3-a5d1-43d4-afc3-27e4258cc574",
+    link: "https://firebasestorage.googleapis.com/v0/b/pdf-resouraces.appspot.com/o/Salud%20desafiante%2FMenu%2F07.pdf?alt=media&token=1e64f0ac-8b14-48b3-82d5-14cf7adaaf70",
+  },
+];
+
+export const dataMenuTwo = [
+  {
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/saluddesafiante.appspot.com/o/menu%20semanal%2Fmenu-01.2.webp?alt=media&token=0bf20243-ebdb-43a9-8769-8f9b50fbd4db",
+    link: "https://firebasestorage.googleapis.com/v0/b/pdf-resouraces.appspot.com/o/Salud%20desafiante%2FMenu%2F01-2.pdf?alt=media&token=d04ffe9d-175f-43bf-9f9c-028dde559577",
+  },
+  {
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/saluddesafiante.appspot.com/o/menu%20semanal%2Fmenu-02.2.webp?alt=media&token=fe847401-fc54-48ee-a5d5-e6d588816c94",
+    link: "https://firebasestorage.googleapis.com/v0/b/pdf-resouraces.appspot.com/o/Salud%20desafiante%2FMenu%2F02-02.pdf?alt=media&token=182e974a-d695-4f65-a852-1a4a3767e503",
+  },
+  {
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/saluddesafiante.appspot.com/o/menu%20semanal%2Fmenu-03.2.webp?alt=media&token=11cb84f5-8ca5-49ca-a431-93e5e78f5091",
+    link: "https://firebasestorage.googleapis.com/v0/b/pdf-resouraces.appspot.com/o/Salud%20desafiante%2FMenu%2F03-02.pdf?alt=media&token=70251ed3-640e-4165-bf6c-941423fd3d08",
+  },
+];
---
+++ components/organisms/ChallengingHealth/data/tips.ts
@@ -0,0 +1,26 @@
+export const dataTips = [
+  {
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/saluddesafiante.appspot.com/o/tips%2FfrutosSecos.webp?alt=media&token=1668c439-7da7-4a48-8ce5-1f022130028f",
+    title: "¿Es necesario activar semillas, frutos secos y legumbres?",
+    link: "https://firebasestorage.googleapis.com/v0/b/pdf-resouraces.appspot.com/o/Salud%20desafiante%2FTips%2FActivar%20semillas%2C%20frutos%20secos%20y%20legumbres.pdf?alt=media&token=ca66c1eb-af96-4dd5-9452-ac4a4967d5cc",
+  },
+  {
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/saluddesafiante.appspot.com/o/tips%2FimportanciaProteinas.webp?alt=media&token=b699d97e-627b-4f3f-adbf-a217f7b747e0",
+    title: "¿Por qué son importantes las proteínas?",
+    link: "https://firebasestorage.googleapis.com/v0/b/pdf-resouraces.appspot.com/o/Salud%20desafiante%2FTips%2FPor%20qu%C3%A9%20son%20importantes%20las%20prote%C3%ADnas.pdf?alt=media&token=2c193cc9-2e8a-49dc-940d-261527a46995",
+  },
+  {
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/saluddesafiante.appspot.com/o/tips%2Fglucemico.webp?alt=media&token=353ee707-d5f3-4814-b5f7-768306e328a6",
+    title: "Índice Glucémico",
+    link: "https://firebasestorage.googleapis.com/v0/b/pdf-resouraces.appspot.com/o/Salud%20desafiante%2FTips%2F%C3%8DNDICE%20GLUC%C3%89MICO.pdf?alt=media&token=63c36e0d-39a7-4852-9282-fda83b90dc32",
+  },
+  {
+    image:
+      "https://firebasestorage.googleapis.com/v0/b/saluddesafiante.appspot.com/o/tips%2FexcesoDeSodio.webp?alt=media&token=02ce0236-afa1-46e6-b024-fbaae4fb1132",
+    title: "¿Qué pasa si me excedo en el consumo de sodio?",
+    link: "https://firebasestorage.googleapis.com/v0/b/pdf-resouraces.appspot.com/o/Salud%20desafiante%2FTips%2FQu%C3%A9%20pasa%20si%20me%20excedo%20en%20el%20consumo%20de%20sodio.pdf?alt=media&token=72c454b0-70de-4a4e-a957-91da86a4a29f",
+  },
+];
