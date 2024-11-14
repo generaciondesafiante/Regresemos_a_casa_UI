6af60cb17d0cdb9779a8ce686899bf2dcb09326e
daniel mayo <daniielmayo.mayo@gmail.com>
2024-11-14 07:23:18 -0500

    performance of the application page home

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.imgur.com",
+      },
+      {
+        protocol: "https",
+        hostname: "static.wixstatic.com",
+      },
+    ],
+  },
+};
 
 module.exports = nextConfig;
