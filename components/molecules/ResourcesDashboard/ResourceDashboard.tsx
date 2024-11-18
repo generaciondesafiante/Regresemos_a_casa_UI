af56e3faa59d4f356fd2243aa6f5a2cec5d8b0ef
Daniielmayo <79063734+Daniielmayo@users.noreply.github.com>
2024-11-18 10:32:34 -0500

    pull origin develop (#216)

--- components/molecules/ResourcesDashboard/ResourceDashboard.tsx
+++ components/molecules/ResourcesDashboard/ResourceDashboard.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import styles from "./ResourceDashboard.module.css";
+import Image from "next/image";
 export const ResourceDashboard = () => {
-  const FILLER_CONTENT_IMG =
+  const FILLER_CONTENT_IMG1 =
     "https://static.wixstatic.com/media/d166cc_4cc837baf9254000a0f3963193c6b07a~mv2.jpg/v1/fill/w_368,h_195,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Romanos%2011111.jpg";
 
   return (
@@ -10,34 +11,216 @@ export const ResourceDashboard = () => {
         Recursos para ti
       </h3>
       <div className={styles["dashboardUserWelcome-resourcesContainer"]}>
-        <img src={FILLER_CONTENT_IMG} alt="" />
-        <img src={FILLER_CONTENT_IMG} alt="" />
-        <img src={FILLER_CONTENT_IMG} alt="" />
-        <img src={FILLER_CONTENT_IMG} alt="" />
-        <img src={FILLER_CONTENT_IMG} alt="" />
-        <img src={FILLER_CONTENT_IMG} alt="" />
-        <img src={FILLER_CONTENT_IMG} alt="" />
-        <img src={FILLER_CONTENT_IMG} alt="" />
-        <img src={FILLER_CONTENT_IMG} alt="" />
-        <img src={FILLER_CONTENT_IMG} alt="" />
-        <img src={FILLER_CONTENT_IMG} alt="" />
-        <img src={FILLER_CONTENT_IMG} alt="" />
-        <img src={FILLER_CONTENT_IMG} alt="" />
-        <img src={FILLER_CONTENT_IMG} alt="" />
-        <img src={FILLER_CONTENT_IMG} alt="" />
-        <img src={FILLER_CONTENT_IMG} alt="" />
-        <img src={FILLER_CONTENT_IMG} alt="" />
-        <img src={FILLER_CONTENT_IMG} alt="" />
-        <img src={FILLER_CONTENT_IMG} alt="" />
-        <img src={FILLER_CONTENT_IMG} alt="" />
-        <img src={FILLER_CONTENT_IMG} alt="" />
-        <img src={FILLER_CONTENT_IMG} alt="" />
-        <img src={FILLER_CONTENT_IMG} alt="" />
-        <img src={FILLER_CONTENT_IMG} alt="" />
-        <img src={FILLER_CONTENT_IMG} alt="" />
-        <img src={FILLER_CONTENT_IMG} alt="" />
-        <img src={FILLER_CONTENT_IMG} alt="" />
-        <img src={FILLER_CONTENT_IMG} alt="" />
+        <Image
+          src={
+            "https://static.wixstatic.com/media/d166cc_4cc837baf9254000a0f3963193c6b07a~mv2.jpg/v1/fill/w_368,h_195,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Romanos%2011111.jpg"
+          }
+          alt="Resource dashboard"
+          width={128}
+          height={100}
+          layout="responsive"
+          quality={80}
+        />
+        <Image
+          src={
+            "https://static.wixstatic.com/media/d166cc_4cc837baf9254000a0f3963193c6b07a~mv2.jpg/v1/fill/w_368,h_195,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Romanos%2011111.jpg"
+          }
+          alt="Resource dashboard"
+          width={128}
+          height={100}
+        />
+        <Image
+          src={
+            "https://static.wixstatic.com/media/d166cc_4cc837baf9254000a0f3963193c6b07a~mv2.jpg/v1/fill/w_368,h_195,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Romanos%2011111.jpg"
+          }
+          alt="Resource dashboard"
+          width={128}
+          height={100}
+        />
+        <Image
+          src={
+            "https://static.wixstatic.com/media/d166cc_4cc837baf9254000a0f3963193c6b07a~mv2.jpg/v1/fill/w_368,h_195,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Romanos%2011111.jpg"
+          }
+          alt="Resource dashboard"
+          width={128}
+          height={100}
+        />
+        <Image
+          src={
+            "https://static.wixstatic.com/media/d166cc_4cc837baf9254000a0f3963193c6b07a~mv2.jpg/v1/fill/w_368,h_195,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Romanos%2011111.jpg"
+          }
+          alt="Resource dashboard"
+          width={128}
+          height={100}
+        />
+        <Image
+          src={
+            "https://static.wixstatic.com/media/d166cc_4cc837baf9254000a0f3963193c6b07a~mv2.jpg/v1/fill/w_368,h_195,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Romanos%2011111.jpg"
+          }
+          alt="Resource dashboard"
+          width={128}
+          height={100}
+        />
+        <Image
+          src={
+            "https://static.wixstatic.com/media/d166cc_4cc837baf9254000a0f3963193c6b07a~mv2.jpg/v1/fill/w_368,h_195,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Romanos%2011111.jpg"
+          }
+          alt="Resource dashboard"
+          width={128}
+          height={100}
+        />
+        <Image
+          src={
+            "https://static.wixstatic.com/media/d166cc_4cc837baf9254000a0f3963193c6b07a~mv2.jpg/v1/fill/w_368,h_195,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Romanos%2011111.jpg"
+          }
+          alt="Resource dashboard"
+          width={128}
+          height={100}
+        />
+        <Image
+          src={
+            "https://static.wixstatic.com/media/d166cc_4cc837baf9254000a0f3963193c6b07a~mv2.jpg/v1/fill/w_368,h_195,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Romanos%2011111.jpg"
+          }
+          alt="Resource dashboard"
+          width={128}
+          height={100}
+        />
+        <Image
+          src={
+            "https://static.wixstatic.com/media/d166cc_4cc837baf9254000a0f3963193c6b07a~mv2.jpg/v1/fill/w_368,h_195,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Romanos%2011111.jpg"
+          }
+          alt="Resource dashboard"
+          width={128}
+          height={100}
+        />
+        <Image
+          src={
+            "https://static.wixstatic.com/media/d166cc_4cc837baf9254000a0f3963193c6b07a~mv2.jpg/v1/fill/w_368,h_195,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Romanos%2011111.jpg"
+          }
+          alt="Resource dashboard"
+          width={128}
+          height={100}
+        />
+        <Image
+          src={
+            "https://static.wixstatic.com/media/d166cc_4cc837baf9254000a0f3963193c6b07a~mv2.jpg/v1/fill/w_368,h_195,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Romanos%2011111.jpg"
+          }
+          alt="Resource dashboard"
+          width={128}
+          height={100}
+        />
+        <Image
+          src={
+            "https://static.wixstatic.com/media/d166cc_4cc837baf9254000a0f3963193c6b07a~mv2.jpg/v1/fill/w_368,h_195,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Romanos%2011111.jpg"
+          }
+          alt="Resource dashboard"
+          width={128}
+          height={100}
+        />
+        <Image
+          src={
+            "https://static.wixstatic.com/media/d166cc_4cc837baf9254000a0f3963193c6b07a~mv2.jpg/v1/fill/w_368,h_195,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Romanos%2011111.jpg"
+          }
+          alt="Resource dashboard"
+          width={128}
+          height={100}
+        />
+        <Image
+          src={
+            "https://static.wixstatic.com/media/d166cc_4cc837baf9254000a0f3963193c6b07a~mv2.jpg/v1/fill/w_368,h_195,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Romanos%2011111.jpg"
+          }
+          alt="Resource dashboard"
+          width={128}
+          height={100}
+        />
+        <Image
+          src={
+            "https://static.wixstatic.com/media/d166cc_4cc837baf9254000a0f3963193c6b07a~mv2.jpg/v1/fill/w_368,h_195,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Romanos%2011111.jpg"
+          }
+          alt="Resource dashboard"
+          width={128}
+          height={100}
+        />
+        <Image
+          src={
+            "https://static.wixstatic.com/media/d166cc_4cc837baf9254000a0f3963193c6b07a~mv2.jpg/v1/fill/w_368,h_195,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Romanos%2011111.jpg"
+          }
+          alt="Resource dashboard"
+          width={128}
+          height={100}
+        />
+        <Image
+          src={
+            "https://static.wixstatic.com/media/d166cc_4cc837baf9254000a0f3963193c6b07a~mv2.jpg/v1/fill/w_368,h_195,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Romanos%2011111.jpg"
+          }
+          alt="Resource dashboard"
+          width={128}
+          height={100}
+        />
+        <Image
+          src={
+            "https://static.wixstatic.com/media/d166cc_4cc837baf9254000a0f3963193c6b07a~mv2.jpg/v1/fill/w_368,h_195,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Romanos%2011111.jpg"
+          }
+          alt="Resource dashboard"
+          width={128}
+          height={100}
+        />
+        <Image
+          src={
+            "https://static.wixstatic.com/media/d166cc_4cc837baf9254000a0f3963193c6b07a~mv2.jpg/v1/fill/w_368,h_195,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Romanos%2011111.jpg"
+          }
+          alt="Resource dashboard"
+          width={128}
+          height={100}
+        />
+        <Image
+          src={
+            "https://static.wixstatic.com/media/d166cc_4cc837baf9254000a0f3963193c6b07a~mv2.jpg/v1/fill/w_368,h_195,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Romanos%2011111.jpg"
+          }
+          alt="Resource dashboard"
+          width={128}
+          height={100}
+        />
+        <Image
+          src={
+            "https://static.wixstatic.com/media/d166cc_4cc837baf9254000a0f3963193c6b07a~mv2.jpg/v1/fill/w_368,h_195,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Romanos%2011111.jpg"
+          }
+          alt="Resource dashboard"
+          width={128}
+          height={100}
+        />
+        <Image
+          src={
+            "https://static.wixstatic.com/media/d166cc_4cc837baf9254000a0f3963193c6b07a~mv2.jpg/v1/fill/w_368,h_195,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Romanos%2011111.jpg"
+          }
+          alt="Resource dashboard"
+          width={128}
+          height={100}
+        />
+        <Image
+          src={
+            "https://static.wixstatic.com/media/d166cc_4cc837baf9254000a0f3963193c6b07a~mv2.jpg/v1/fill/w_368,h_195,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Romanos%2011111.jpg"
+          }
+          alt="Resource dashboard"
+          width={128}
+          height={100}
+        />
+        <Image
+          src={
+            "https://static.wixstatic.com/media/d166cc_4cc837baf9254000a0f3963193c6b07a~mv2.jpg/v1/fill/w_368,h_195,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Romanos%2011111.jpg"
+          }
+          alt="Resource dashboard"
+          width={128}
+          height={100}
+        />
+        <Image
+          src={
+            "https://static.wixstatic.com/media/d166cc_4cc837baf9254000a0f3963193c6b07a~mv2.jpg/v1/fill/w_368,h_195,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/Romanos%2011111.jpg"
+          }
+          alt="Resource dashboard"
+          width={128}
+          height={100}
+        />
       </div>
     </div>
   );
